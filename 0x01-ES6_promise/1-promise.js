cb072fadfe639063ff7f7cfc258abf6d6a4b56d1
chimaskyy <chiamakahj@gmail.com>
2024-04-25 05:52:17 +0100

    fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
-/* eslint-disable */
 export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            return resolve({
-                status: 200,
-                body: 'Success'
-            });
-        }
-        return reject('The fake API is not working currently');
-    });
+  return new Promise((resolve, reject) => {
+    if (success) {
+      return resolve({
+        status: 200,
+        body: 'Success',
+      });
+    }
+    // eslint-disable-next-line prefer-promise-reject-errors
+    return reject('The fake API is not working currently');
+  });
 }
--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,7 @@
 /* eslint-disable */
 export default function handleResponseFromAPI(promise) {
-    return new Promise((resolve, reject) => {
-        if (promise) {
-            resolve({
-                status: 200,
-                body: 'success'
-            });
-            console.log("Got a response from the API");
-        } else {
-            reject(Error(''));
-        }
-    });
+  return promise
+    .then(() => ({ status: 200, body: "success" }))
+    .catch(() => new Error())
+    .finally(() => console.log("Got a response from the API"));
 }
