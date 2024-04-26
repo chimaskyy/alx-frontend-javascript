a8addcc13d8826ac540f9915d0b2199f7f483651
chimaskyy <chiamakahj@gmail.com>
2024-04-26 17:09:29 +0100

    getter and setter

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,7 +3,7 @@ import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
   const rooms = [];
-  rooms.push(new ClassRoom(10));
+  rooms.push(new ClassRoom(19));
   rooms.push(new ClassRoom(20));
   rooms.push(new ClassRoom(34));
 
---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,40 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new Error('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new Error('Stdent must be an array');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    this._students = value;
+  }
+}
