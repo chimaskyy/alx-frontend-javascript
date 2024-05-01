e61aeb950a23e5265bc36fe001bb893c53f91f02
chimaskyy <chiamakahj@gmail.com>
2024-05-01 05:28:47 +0100

    set has

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,9 @@
+export default function hasValuesFromArray(set_, array_) {
+  let bool = true;
+  array_.map((val) => {
+    if (!set_.has(val)) {
+      bool = false;
+    }
+  });
+  return bool;
+}
